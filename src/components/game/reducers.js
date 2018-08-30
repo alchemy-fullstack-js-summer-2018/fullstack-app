3b9a1ea7f785ab662e65e57be1e6574249e2fd8c
rbtprograms <rbt61690@gmail.com>
2018-08-30 16:37:07 -0700

    implementing moves reducer and selector

--- src/components/game/reducers.js
+++ src/components/game/reducers.js
@@ -1,6 +1,13 @@
 export const GAME_LOAD = 'GAME_LOAD';
+export const MOVE_LOAD = 'MOVE_LOAD';
+
 export const getGame = state => state.game;
+export const getMoves = state => state.moves;
 
 export function game(state = null, { type, payload }) {
   return type === GAME_LOAD ? payload : state;
+}
+
+export function moves(state = [], { type, payload }) {
+  return type === MOVE_LOAD ? payload : state;
 }
