c48f6aeb0d02ac11953de3a53d684d9b73003a4a
rbtprograms <rbt61690@gmail.com>
2018-09-01 14:51:10 -0700

    dummy commit to start dev

--- src/services/request.js
+++ src/services/request.js
@@ -17,3 +17,5 @@ export const get = url => request(url);
 export const post = (url, data) => request(url, { method: 'POST', headers }, data);
 export const put = (url, data) => request(url, { method: 'PUT', headers }, data);
 export const del = (url, data) => request(url, { method: 'DELETE' }, data);
+
+// dummy commit for dev
