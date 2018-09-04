7aaefd734f7870bdf3dcd95ba5e2b5c9b612f902
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-09-04 11:05:19 -0700

    Add LeaderboardDisplay component

---
+++ src/components/game/LeaderboardDisplay.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class LeaderboardDisplay extends Component {
+  static propTypes = {
+    leaders: PropTypes.array.isRequired
+  };
+
+  render() { 
+    const { leaders } = this.props;
+
+    return (
+      <div>
+        <h2>Top 5 Most Successful Generals</h2>
+        <p>{leaders[0].name}: {leaders[0].wins}</p>
+        <p>{leaders[1].name}: {leaders[1].wins}</p>
+        <p>{leaders[2].name}: {leaders[2].wins}</p>
+        <p>{leaders[3].name}: {leaders[3].wins}</p>
+        <p>{leaders[4].name}: {leaders[4].wins}</p>
+      </div>
+    );
+  }
+}
+ 
+export default LeaderboardDisplay;
