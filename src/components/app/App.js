f7b82cc779b9f080b913e88d3e67dc80c5c5f21f
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-09-04 12:03:10 -0700

    Update App component to use Leaderboard component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -10,6 +10,7 @@ import Home from './Home';
 import Game from '../game/Game';
 import Auth from '../auth/Auth';
 import Dashboard from '../dashboard/Dashboard';
+import LeaderboardDisplay from '../game/LeaderboardDisplay';
 // import styles from './App.css'
 
 class App extends Component {
@@ -39,7 +40,7 @@ class App extends Component {
                 <Route exact path="/" component={Home}/>
                 <Route path="/auth" component={Auth}/>
                 <Route exact path="/dashboard" component={Dashboard}/>
-                <Route exact path="/leaderboard" component={Home}/>
+                <Route exact path="/leaderboard" component={LeaderboardDisplay}/>
 
                 <PrivateRoute path="/games/:gameKey" component={Game}/>
                 <Redirect to="/"/>
