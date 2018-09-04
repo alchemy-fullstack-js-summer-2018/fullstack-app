a5799c91c122909a24b0fa8ad5945220dd90c9fe
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-09-04 14:41:59 -0700

    Tweak leaderboard component

--- src/components/game/LeaderboardDisplay.js
+++ src/components/game/LeaderboardDisplay.js
@@ -2,23 +2,31 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getLeaders } from '../dashboard/reducers';
+import { loadLeaders } from '../dashboard/actions';
 
 class LeaderboardDisplay extends Component {
   static propTypes = {
-    leaders: PropTypes.array.isRequired
+    leaders: PropTypes.array,
+    loadLeaders: PropTypes.func.isRequired
   };
 
+  componentDidMount() {
+    // const { loadLeaders } = this.props;
+    console.log(this.props);
+    this.props.loadLeaders();
+  }
+
   render() { 
-    const { leaders } = this.props;
+    // const { leaders } = this.props;
 
     return (
       <div>
         <h2>Top 5 Most Successful Generals</h2>
-        <p>{leaders[0].name}: {leaders[0].wins}</p>
+        {/* <p>{leaders[0].name}: {leaders[0].wins}</p>
         <p>{leaders[1].name}: {leaders[1].wins}</p>
         <p>{leaders[2].name}: {leaders[2].wins}</p>
         <p>{leaders[3].name}: {leaders[3].wins}</p>
-        <p>{leaders[4].name}: {leaders[4].wins}</p>
+        <p>{leaders[4].name}: {leaders[4].wins}</p> */}
       </div>
     );
   }
@@ -27,5 +35,6 @@ class LeaderboardDisplay extends Component {
 export default connect(
   state => ({
     leaders: getLeaders(state)
-  }, null)
+  }),
+  { loadLeaders }
 )(LeaderboardDisplay);
